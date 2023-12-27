0ebea71fee27049d5fe08896997911bce27f3137
basslhm <contact@kompo.io>
2023-12-27 16:52:07 -0300

    setup main folder structure

---
+++ src/KompoAuthServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Condoedge\Auth;
+
+use Illuminate\Support\ServiceProvider;
+
+class KompoAuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //$this->mergeConfigFrom(__DIR__.'/../../config/kompo.php', 'kompo');
+
+        //$this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
+
+        $this->loadJSONTranslationsFrom(__DIR__.'/../../resources/lang');
+
+        //$this->loadViewsFrom(__DIR__.'/../../resources/views', 'kompo');
+
+         //Usage: php artisan vendor:publish --provider="Kompo\KompoServiceProvider"
+        $this->publishes([
+            __DIR__.'/../../config/kompo.php' => config_path('kompo.php'),
+        ]);
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}

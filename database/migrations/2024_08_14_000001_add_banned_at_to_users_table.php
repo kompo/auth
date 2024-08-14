392d7a237c5c32c4ef053e22b0f4e21a1601e706
Ezequiel <tavelliezequiel@gmail.com>
2024-08-14 19:36:58 -0300

    Banned and blocked option

---
+++ database/migrations/2024_08_14_000001_add_banned_at_to_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->timestamp('banned_at')->nullable();
+            $table->timestamp('blocked_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('banned_at');
+            $table->dropColumn('blocked_at');
+        });
+    }
+};

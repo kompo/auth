92379d5e1a5415ba49976e9cd5381e483b7d09ef
Ezequiel <tavelliezequiel@gmail.com>
2025-02-26 11:56:25 -0300

    New FacadeEnum concept

---
+++ src/Facades/FacadeEnum.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Kompo\Auth\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+abstract class FacadeEnum extends Facade
+{
+    public static function getEnumClass()
+    {
+        return static::getFacadeRoot();
+    }
+
+    public static function __callStatic($method, $args)
+    {
+        $instance = static::getEnumClass();
+
+        if (! $instance) {
+            throw new RuntimeException('A facade root has not been set.');
+        }
+
+        return $instance::$method(...$args);
+    }
+}

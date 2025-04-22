de20dc4991b659a875199522b1a8a7924c0c5d5a
Benoit Babin <bbabin@decizif.com>
2025-04-22 09:09:03 -0400

    Translation

--- src/Helpers/exports.php
+++ src/Helpers/exports.php
@@ -2,5 +2,5 @@
 
 function _ExcelExportButton()
 {
-    return _Link('EXCEL')->icon('download')->outlined()->class('mb-4')->selfPost('exportToExcel')->withAllFormValues()->inModal();
+    return _Link('Excel')->icon('download')->outlined()->class('mb-4 !px-3')->selfPost('exportToExcel')->withAllFormValues()->inModal();
 }

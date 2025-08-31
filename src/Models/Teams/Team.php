676e62767106cca36be8bde10577ed301a61ebc7
Ezequiel <tavelliezequiel@gmail.com>
2025-08-31 19:11:05 -0300

    seeing if deleting flush tags is still working

--- src/Models/Teams/Team.php
+++ src/Models/Teams/Team.php
@@ -52,14 +52,6 @@ class Team extends Model
             ]);
 
             $this->addedBy?->clearPermissionCache();
-            // $store = Cache::getStore();
-
-            // $connection = $store->connection();
-            // $redis = \Illuminate\Support\Facades\Redis::connection($connection->getName());
-            // $prefix = $store->getPrefix();
-            // Cache::tags([self::CACHE_TAG])->forget('team_role_permissions.757767');
-            Cache::flushTags(['permissions-v2']);
-            // dd($redis->keys("*"));
         }
     }
 

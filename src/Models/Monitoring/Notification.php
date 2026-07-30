1eeff638a3fc1998f1a58a375f1dfa0f8ab41ef0
Ezequiel <tavelliezequiel@gmail.com>
2026-07-30 20:26:37 -0300

    More dynamic display of buttons

--- src/Models/Monitoring/Notification.php
+++ src/Models/Monitoring/Notification.php
@@ -113,15 +113,15 @@ class Notification extends Model implements NoTeamScope, HasOwnedRecords
         $buttons = is_array($buttons) ? array_filter($buttons) : ($buttons ? [$buttons] : []);
 
         $buttonsBlock = empty($buttons) ? null : _Flex(
-            ...collect($buttons)->map(fn ($b) => $b->class('flex-1'))->all(),
-        )->class('gap-2 flex-wrap flex-1');
+            ...collect($buttons)->map(fn ($b) => $b->class('flex-1')->when(count($buttons) === 1, fn ($b) => $b->class('xl:flex-[unset]')))->all(),
+        )->class('gap-2 flex-wrap flex-1')->when(count($buttons) === 1, fn ($b) => $b->class('xl:!justify-end'));
 
         return _Rows(
             _Html($title)->class('mb-3'),
             _FlexBetween(
                 $buttonsBlock,
                 !$hasReminderButton ? null : $this->reminderDropdown()?->class('flex-1'),
-            )->class('gap-4 flex-wrap')
+            )->class('gap-4 flex-wrap xl:!justify-end')
         );
     }
 
